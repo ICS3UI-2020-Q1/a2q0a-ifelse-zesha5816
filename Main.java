e51b82c2d48e25d11835de5f6d90b104c3aa93b5
zesha5816 <zesha5816@wrdsb.ca>
2020-09-15 13:01:23 +0000

    finished example question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *  This program will let the user know if they are old enough to vote
+ * @author Mr. Lamont
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // store how old you must be to get your license
+    final int LICENSE_AGE = 16;
+
+    // store how old you must be to vote
+    final int VOTING_AGE = 18;
+
+    // get the user's age and store it for later
+    System.out.println("Please enter your age:");
+    int age = input.nextInt();
+
+    // check to see if they are old enough to get a license or vote
+    if (age >= VOTING_AGE) {
+      System.out.println("You are old enough to drive and vote!");
+    } else if (age >= LICENSE_AGE) {
+      System.out.println("You can get your driver's license!");
+    } else {
+      System.out.println("Sorry, you are not allowed to drive or vote.");
+    }
+
   }
 }
